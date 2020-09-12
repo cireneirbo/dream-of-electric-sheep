b5bc122fc099809e0bc4869304c20332e2341908
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-09-12 14:09:16 -0400

    init commit

---
+++ src/test/java/com/dreamofelectricsheep/dreamofelectricsheep/DreamOfElectricSheepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamofelectricsheep.dreamofelectricsheep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamOfElectricSheepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
