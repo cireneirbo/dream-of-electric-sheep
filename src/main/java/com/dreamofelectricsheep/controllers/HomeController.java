41d740db39e4b5fb0c272509f8382dd2f1785fe4
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-09-12 16:24:07 -0400

    added some templates and a controller

---
+++ src/main/java/com/dreamofelectricsheep/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.dreamofelectricsheep.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+}
