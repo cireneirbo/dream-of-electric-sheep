7065a9f1f123cf953aaa43c033342acc316d3de5
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-10-05 20:38:10 -0400

    added auth0Provider tags to index.js

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,14 +4,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <Auth0Provider
+    domain="YOUR_DOMAIN"
+    clientId="YOUR_CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>,
   rootElement);
 
 registerServiceWorker();
