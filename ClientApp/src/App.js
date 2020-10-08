8ed5f0acada293e9ad4ac87929e440561819ba9c
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-10-07 22:55:26 -0400

    Update Auth0Provider in index.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,7 +4,6 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-import { Profile } from './components/LoginButton';
 import './custom.css'
 
 export default class App extends Component {
@@ -14,7 +13,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/login' component={Profile} />
+        {/* <Route path='/login' component={Profile} /> */}
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
         
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,21 +4,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Auth0Provider } from "@auth0/auth0-react";
+// import Auth0ProviderWithHistory from './auth/Auth0ProviderWithHistory';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="YOUR_DOMAIN"
-    clientId="YOUR_CLIENT_ID"
-    redirectUri={window.location.origin}
-  >
-    <BrowserRouter basename={baseUrl}>
-      <App />
-    </BrowserRouter>
-  </Auth0Provider>,
+  <BrowserRouter basename={baseUrl}>
+    <Auth0Provider
+      domain={process.env.YOUR_DOMAIN}
+      clientId={process.env.YOUR_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <App /> 
+    </Auth0Provider>
+  </BrowserRouter>,
   rootElement);
 
 registerServiceWorker();
