37e06842d8263a139810dfe1e388b6f8aa240ee4
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-10-13 17:50:39 -0400

    harvesting a free to use theme in progress

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -6,11 +6,12 @@ import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { useAuth0 } from "@auth0/auth0-react";
 import { ProtectedRoute } from './auth/protected-route';
-import './custom.css'
 import { NavMenu } from "./components/NavMenu";
 import { Loading } from "./components/loading";
 import { Profile } from "./views/profile";
 import { FetchCrypto } from './components/FetchCrypto';
+// import './custom.css';
+import './styles/assets/css/main.css';
 
 const App = () => {
   const { isLoading } = useAuth0();
@@ -20,9 +21,9 @@ const App = () => {
   }
 
   return (
-    <div id="app" className="d-flex flex-column h-100">
+    <div id="logo" className="d-flex flex-column h-100">
       <NavMenu />
-      <div className="container flex-grow-1">
+      <div id="nav" className="container flex-grow-1">
         <Switch>
           <Route path="/" exact component={Home} />
           <ProtectedRoute path="/counter" component={Counter} />
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Auth0ProviderWithHistory } from './auth/auth0-provider-with-history.js';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
