3d0d88e6e3027a0bf42a6474a37e009e3c801589
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-01-28 20:48:24 -0500

    added some coin market cap code

--- Controllers/CryptoPricesController.cs
+++ Controllers/CryptoPricesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Web;
 
 namespace dream_of_electric_sheep.Controllers
 {
@@ -11,18 +13,17 @@ namespace dream_of_electric_sheep.Controllers
     [Route("cryptoprices")]
     public class CryptoPricesController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        /*private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" //update
-        };
-
+        };*/
         private readonly ILogger<CryptoPricesController> _logger;
 
         public CryptoPricesController(ILogger<CryptoPricesController> logger)
         {
             _logger = logger;
         }
-
+/*
         [HttpGet]
         public IEnumerable<CryptoPrices> Get()
         {
@@ -34,6 +35,38 @@ namespace dream_of_electric_sheep.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }*/
+
+        private static string API_KEY = "COIN_MARKET_CAP_API_KEY";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine(makeAPICall());
+            }
+            catch (WebException e)
+            {
+            Console.WriteLine(e.Message);
+            }
         }
+
+        static string makeAPICall()
+            {
+                var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+                queryString["start"] = "1";
+                queryString["limit"] = "5000";
+                queryString["convert"] = "USD";
+
+                URL.Query = queryString.ToString();
+
+                var client = new WebClient();
+                client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+                client.Headers.Add("Accepts", "application/json");
+                return client.DownloadString(URL.ToString());
+
+            }
     }
 }
